6b5cf1423f00c5d80f900b5be0f909afa87db44f
NikolaNikolaev <nikola_creators@abv.bg>
2021-02-14 12:55:56 +0200

    Render login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,5 +4,8 @@ const router = Router();
 // Register
 router.get('/register', (req, res) => { res.render('../views/auth/register.hbs'); });
 
+// Login
+router.get('/login', (req, res) => { res.render('../views/auth/login.hbs'); });
+
 
 module.exports = router;
