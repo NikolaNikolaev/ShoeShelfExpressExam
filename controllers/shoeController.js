5f850048a021ebc3128923db8136ffd0fab044a6
NikolaNikolaev <nikola_creators@abv.bg>
2021-02-14 16:59:47 +0200

    Render create new shoe offer page

--- controllers/shoeController.js
+++ controllers/shoeController.js
@@ -4,5 +4,8 @@ const router = Router();
 // Home page
 router.get('/', (req, res) => { res.render('../views/home.hbs'); });
 
+// Create Shoe Offer
+router.get('/create', (req, res) => { res.render('../views/shoes/create'); });
+
 
 module.exports = router;
